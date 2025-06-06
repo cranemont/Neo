a2b0e2e824eaf4d7a8cb01ba8023d0ee51172ed2
Haksan <cranemont@gmail.com>
2025-06-07 00:58:25 +0900

    feat: setup logger

---
+++ packages/explorer/src/logger.ts
@@ -0,0 +1,22 @@
+import winston from 'winston';
+
+const label = '[EXPLORER]';
+
+const alignColorsAndTime = winston.format.combine(
+  winston.format.label({
+    label: `\x1b[48;5;189;30m${label}\x1b[0m`,
+  }),
+  winston.format.timestamp({
+    format: 'YY-MM-DD HH:mm:ss',
+  }),
+  winston.format.colorize({ level: true }),
+  winston.format.printf((info) => ` ${info.timestamp} ${info.label} ${info.level} : ${info.message}`),
+);
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(alignColorsAndTime),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
